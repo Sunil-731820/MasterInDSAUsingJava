b3db28b187390eb8a33a560ac95daefeef558aa1
Sunil-731820 <sunilkumargupta1170@gmail.com>
2023-01-03 15:05:10 +0530

    code

---
+++ src/main/java/com/java/ds/JUnitTesting/DivisionTestCase.java
@@ -0,0 +1,20 @@
+package com.java.ds.JUnitTesting;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+public class DivisionTestCase {
+
+//	called Only once before Starting  all the Test Cases
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("In before Class Method");
+	}
+	
+//	called only Once After the Finishing all The Test Cases
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("In AfterClass Method");
+	}
+
+	
+	
+}
