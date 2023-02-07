f28ad7c4878632e7923841e1f0cba2bae90bfea4
Sunil-731820 <sunilkumargupta1170@gmail.com>
2023-02-07 15:06:47 +0530

    coded

---
+++ src/main/java/CollectionsInJava/ListCollectionInJava/Stack/FirstStackExample.java
@@ -0,0 +1,36 @@
+package CollectionsInJava.ListCollectionInJava.Stack;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class FirstStackExample {
+    public static void main(String[] args) {
+        System.out.println("This Is The First EXample Of The Stack ");
+
+        /*
+         * 1 :
+         * Stack
+         * The stack is the subclass of Vector. It implements the last-in-first-out data
+         * structure, i.e., Stack. The
+         * stack contains all of the methods of Vector class and also provides its
+         * methods like boolean push(),
+         * boolean peek(), boolean push(object o), which defines its properties.
+         * Consider the following example.
+         */
+
+         Stack<String> stack = new Stack<>();
+         stack.add("Sunil");
+         stack.add("Harish");
+         stack.add("Sumanth");
+         stack.add("Abhisek");
+         System.out.println("The Stack is ");
+         System.out.println(stack);
+
+         // traversing Through the Iterator
+         Iterator itr = stack.iterator();
+         while(itr.hasNext()){
+            System.out.print(itr.next()+ "  ");
+         }
+
+    }
+}
--- src/main/java/CollectionsInJava/ListCollectionInJava/Vector/FirstVectorExample.java
+++ src/main/java/CollectionsInJava/ListCollectionInJava/Vector/FirstVectorExample.java
@@ -1,10 +1,20 @@
 package CollectionsInJava.ListCollectionInJava.Vector;
 
+import java.util.Iterator;
 import java.util.Vector;
 
 public class FirstVectorExample {
     public static void main(String[] args) {
         System.out.println("This Is the first Example Of The Vector In Java");
+        /*
+         * 1 :
+         * Vector
+         * Vector uses a dynamic array to store the data elements. It is similar to
+         * ArrayList. However, It is
+         * synchronized and contains many methods that are not the part of Collection
+         * framework.
+         * Consider the following example
+         */
         Vector<String> vector = new Vector<>();
         vector.add("Sunil");
         vector.add("Harish");
@@ -12,5 +22,12 @@ public class FirstVectorExample {
         vector.add("Sid");
         System.out.println("The Vector is ");
         System.out.print(vector);
+
+        // Displaying using the Iterator In java
+        System.out.println("Through Iterator is ");
+        Iterator itr = vector.iterator();
+        while(itr.hasNext()){
+            System.out.print(itr.next()+" ");
+        }
     }
 }
