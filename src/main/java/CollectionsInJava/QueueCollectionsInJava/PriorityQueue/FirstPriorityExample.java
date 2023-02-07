e88ba2ced6ec96a4e3d63bb76bfac7d193c28bbc
Sunil-731820 <sunilkumargupta1170@gmail.com>
2023-02-07 15:09:35 +0530

    coded

---
+++ src/main/java/CollectionsInJava/QueueCollectionsInJava/PriorityQueue/FirstPriorityExample.java
@@ -0,0 +1,10 @@
+package CollectionsInJava.QueueCollectionsInJava.PriorityQueue;
+
+public class FirstPriorityExample {
+    public static void main(String[] args) {
+        System.out.println("This IS The EXample Of The Priority Queue");
+        /*
+         * Queue IS Follow The First in First out Principle
+         */
+    }
+}
