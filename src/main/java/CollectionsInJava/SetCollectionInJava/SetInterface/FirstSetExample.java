532fd8e08debde028d08652ad8da1e10fccb0746
Sunil-731820 <sunilkumargupta1170@gmail.com>
2023-02-07 15:56:29 +0530

    coded

---
+++ src/main/java/CollectionsInJava/SetCollectionInJava/SetInterface/FirstSetExample.java
@@ -0,0 +1,25 @@
+package CollectionsInJava.SetCollectionInJava.SetInterface;
+
+public class FirstSetExample {
+    public static void main(String[] args) {
+        System.out.println("This is The Example Of The Set Interface");
+        /*
+         * 1 :
+         * Set Interface
+         * Set Interface in Java is present in java.util package. It extends the
+         * Collection interface. It represents
+         * the unordered set of elements which doesn't allow us to store the duplicate
+         * items. We can store at
+         * most one null value in Set. Set is implemented by HashSet, LinkedHashSet, and
+         * TreeSet.
+         */
+
+         /*
+          * 2 :
+          * Set can be instantiated as:
+          * Set<data-type> s1 = new HashSet<data-type>();
+          * Set<data-type> s2 = new LinkedHashSet<data-type>();
+          * Set<data-type> s3 = new TreeSet<data-type>();
+          */
+    }
+}
