14ebc9f606db6189bc75358658a7b7e66f670415
Sunil-731820 <sunilkumargupta1170@gmail.com>
2023-02-07 14:27:22 +0530

    coded

--- src/main/java/CollectionsInJava/ListCollectionInJava/ArrayList/FirstArrayListExample.java
+++ src/main/java/CollectionsInJava/ListCollectionInJava/ArrayList/FirstArrayListExample.java
@@ -1,7 +1,19 @@
 package CollectionsInJava.ListCollectionInJava.ArrayList;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class FirstArrayListExample {
     public static void main(String[] args) {
         System.out.println("This is The First Example Of The ArrayList in java Collections");
+        ArrayList<Integer> arraylist = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            arraylist.add(i);
+        }
+        //Displaying the Elelement using Iterator in Collections 
+        Iterator itr = arraylist.iterator();
+        while(itr.hasNext()){
+            System.out.print(itr.next()+" ->");
+        }
     }
 }
