d3e31306d01af5cd2b03e3bb8bfc8cbd04d787c4
Sunil-731820 <sunilkumargupta1170@gmail.com>
2023-02-20 00:14:50 +0530

    coded

---
+++ src/main/TaskAssignByMe/MainMethod.java
@@ -0,0 +1,17 @@
+package com.java.ds.TaskAssignByMe;
+
+public class MainCLassMethod {
+    public static void main(String[] args) {
+        // Displaying The List of The Employ
+        OperationOfEmploy operationOfEmploy = new OperationOfEmploy();
+        // 1 : Adding All EMploy To The Array List In Java
+        operationOfEmploy.addEmployInArrayList();
+        // 2 : Sorting All Employ data In Java
+        operationOfEmploy.sortEmployDataInDescendingOrder();
+        // 3 : Adding The one Object At The End Of The Array List In Java
+        operationOfEmploy.addEmployAtTheEndOfTheArrayList();
+        // 4 : Deleting The First object From The Array List in Java
+
+    }
+
+}
